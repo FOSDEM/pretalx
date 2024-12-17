7c798f7387d8b74973f9663873cc1ce6a2e81178
Johan Van de Wauw <johanvdw@fosdem.org>
2024-12-17 22:52:03 +0100

    Fix submission_content
    seems previous commit:
    edb199866c0f39eeea782d1ea9e911781ecb00fc
    was incomplete?

--- src/pretalx/mail/models.py
+++ src/pretalx/mail/models.py
@@ -176,7 +176,7 @@ class MailTemplate(PretalxModel):
                 text = str(self.text).format(**context)
                 reply_to = str(self.reply_to).format(**context) if self.reply_to else ""
                 cc = str(self.cc).format(**context) if self.cc else ""
-                if full_submission_content and "submission" in context_kwargs:
+                if "submission" in context_kwargs:
                     text += "\n\n\n***********\n\n" + str(
                         _("Full proposal content:\n\n")
                     )
