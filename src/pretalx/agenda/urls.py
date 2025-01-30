3761fcd0ad9a31c5c4c5de519d540a5b0db09d4d
Johan Van de Wauw <johanvdw@fosdem.org>
2025-01-31 00:00:53 +0100

    Overwrite default feedback link

--- src/pretalx/agenda/urls.py
+++ src/pretalx/agenda/urls.py
@@ -84,11 +84,11 @@ urlpatterns = [
                     talk.TalkSocialMediaCard.as_view(),
                     name="talk-social",
                 ),
-                path(
-                    "talk/<slug>/feedback/",
-                    talk.FeedbackView.as_view(),
-                    name="feedback",
-                ),
+                #path(
+                #    "talk/<slug>/feedback/",
+                #    talk.FeedbackView.as_view(),
+                #    name="feedback",
+                #),
                 path(
                     "talk/<slug>.ics",
                     talk.SingleICalView.as_view(),
